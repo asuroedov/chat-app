42c5d985957c83fc8608217fdb75dc94610ac0bf
asuroedov <a.suroedov@yandex.ru>
2021-12-05 00:20:54 +0300

    добавлен компонент базовой модалки.

--- chat-app-front/src/Modules/ChatList/ChatList.tsx
+++ chat-app-front/src/Modules/ChatList/ChatList.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import CircleButton from "../../primitives/CircleButton/CircleButton";
 
 import styles from "./styles.module.scss";
+import BaseModal from "../../primitives/BaseModal/BaseModal";
 
 const ChatList = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleOpenCreateNewChatClick = useCallback(() => {
+    setModalVisible(true);
+  }, []);
+
+  const handleCloseCreateNewChat = useCallback(() => {
+    setModalVisible(false);
+  }, []);
+
   return (
-    <div className={styles.wrapper}>
-      <CircleButton />
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <CircleButton onClick={handleOpenCreateNewChatClick} />
+      </div>
+      {modalVisible && <BaseModal closeModal={handleCloseCreateNewChat}>123</BaseModal>}
+    </>
   );
 };
 
