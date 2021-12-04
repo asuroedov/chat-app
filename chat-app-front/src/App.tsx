acbae28eeb35109d15504441ec7350b76b3ddd11
asuroedov <a.suroedov@yandex.ru>
2021-12-04 18:33:34 +0300

    добавлены заготовки компонент для главной страницы

--- chat-app-front/src/App.tsx
+++ chat-app-front/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Auth from "./modules/Auth/Auth";
+import MainPage from "./modules/MainPage/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path={"/"} element={<MainPage />} />
         <Route path={"/auth"} element={<Auth />} />
       </Routes>
     </BrowserRouter>
